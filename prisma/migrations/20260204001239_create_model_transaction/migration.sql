d0ed7170e2e440bba3d12e13883d9ab1e67b4203
Lucas emanoel <lucasemanoel625@gmail.com>
2026-02-04 20:08:31 -0300

    feat: Instalação do Prisma no projeto

---
+++ prisma/migrations/20260204001239_create_model_transaction/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Transaction" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "description" TEXT NOT NULL,
+    "amount" REAL NOT NULL,
+    "type" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "date" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // No estado TRUE ele remove as chaves que não estão no DTO
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
